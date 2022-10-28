2be3233b520a1acd61b80ea9ec7ac5200acbf342
Anastasia Ryzhova <anastasiawarm@gmail.com>
2022-10-28 17:48:46 -0400

    back end completed

--- src/main/java/com/ppm/PpmToolApplication.java
+++ src/main/java/com/ppm/PpmToolApplication.java
@@ -2,8 +2,10 @@ package com.ppm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan
 public class PpmToolApplication {
 
 	public static void main(String[] args) {
